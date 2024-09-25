827cf16e984f22762feacceabf8783f4ffa0973a
Houlaymatou B <houlaymatoubalde@HoulaymatousMBP.lan>
2024-09-24 22:44:43 -0500

    more practice problem

--- 2-Expressions-interactivity/problem_4.cpp
+++ 2-Expressions-interactivity/problem_4.cpp
@@ -8,4 +8,35 @@
 Write a program that can be used as a math tutor for a young student. The program should display two random numbers between 1 and 9 to be added.
 After the student has entered an answer and pressed the [Enter] key, the program should display the correct answer so the student can see if his or her answer is correct.
 */
-// ------------------------- Solution on the way ^^
+
+// -------------------------
+#include <iostream>
+#include <cstdlib> // Header file to get srand() function
+#include <ctime> // Header file needed to use time
+
+using namespace std;
+
+int main(){
+  // Variable declaration
+  int result, number_1, number_2;
+  unsigned seed; // Random generator seed  
+
+
+  seed = time(0); //Using the time function to get a new seed anytime the program runs
+  srand(seed); 
+
+  cout<<"\nWelcome to your favorite math tutor 🧮\n";
+  // generate numbers between 1 and 9
+  number_1 = rand() % 9 + 1;
+  number_2 = rand() % 9 + 1;
+  result= number_1 + number_2;
+
+  // Prompt user
+  cout<<"What is "<<number_1 << "+"<<number_2<<"?\n";
+  cout<<"Type the reseult here: ";
+  cin>>result;
+  
+  // output result
+  cout<<"Great Try! The Correct answer is: "<<result<<"!\n\n";
+  return 0;
+}
---
+++ 3-Making-decisions/problem_1.cpp
@@ -0,0 +1,9 @@
+// Houlaymatou B. | @code_techhb
+
+// -------------------------
+// Chap 4: Conditional statements
+// Goal 🎯: ...
+
+/*Programming Challenges - Problem 1: ....
+Coming soon
+*/
