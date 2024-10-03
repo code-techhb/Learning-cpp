2154bf8197a84655ddccd5e31ad28ca308e43b34
Houlaymatou B <houlaymatoub76@gmail.com>
2024-10-02 23:08:52 -0500

    more practice problem

---
+++ 3-Making-decisions/problem_4.cpp
@@ -0,0 +1,14 @@
+// Houlaymatou B. | @code_techhb
+
+// -------------------------
+// Chap 4: Conditional statements
+// Goal 🎯: Practice if/else and switch statements
+
+/*Programming Challenges - Problem 4: February Days
+The month of February normally has 28 days. But if it is a leap year, February has 29 days. Write a program that asks the user to enter a year. The program should then display the number of days in February that year. Use the following criteria to identify leap years:
+
+Determine whether the year is divisible by 100. If it is, then it is a leap year if and if only it is also divisible by 400. For example, 2000 is a leap year but 2100 is not.
+
+If the year is not divisible by 100, then it is a leap year if and if only it is divisible by 4. For example, 2008 is a leap year but 2009 is not.
+*/
+// -------------------------  Solution coming soon
