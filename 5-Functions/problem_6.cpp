a82fc11bbbc3708a75767fec9a71f36552442ec1
Houlaymatou B <houlaymatoub76@gmail.com>
2024-10-22 16:32:55 -0500

    more practice problems

--- 5-Functions/problem_6.cpp
+++ 5-Functions/problem_6.cpp
@@ -15,13 +15,58 @@ Write a Boolean function named isPrime, which takes an integer as an argument an
 using namespace std;
 
 // Function prototypes
+bool isPrime(int );
 
 // main
 int main(){
+  int number;
+  char go_again = 'y';
 
+  cout<<"\nWelcome to primeChecker\n";
+  do {
+    cout<<"Enter a number: ";
+    cin>>number;
+
+    if (isPrime(number)){
+      cout<<number<<" is indeed a prime number!\n";
+    } else{
+      cout<<number<<" is NOT a prime number!\n";
+    }
+
+    cout<<"\n\nIs there another number you wanna check? Type(y/n): ";
+    cin>>go_again;
+
+    if(tolower(go_again)=='n'){
+      cout<<"Okay, Byeeee!\n\n";
+    }
+  }while(tolower(go_again) != 'n');
+  
   return 0;
 }
 
 // function definiton
+bool isPrime(int n){
+  // because all prime numbers are greater than one
+  if(n <= 1){
+    return false;
+  } 
+  // because 2 is the only even number
+  if(n==2){
+      return true;
+  }
+  // so let's exclude all even numbers after 2
+  if (n%2 == 0){
+    return false;
+  }
 
+  // check for divisibility up to sqrt of n
+  for (int i=3; i*i <= n; i+=2){
+    if (n % i == 0) {
+      // it's not a prime
+      return false;
+    }
+  }
+  // else, we return true bc it's prime
+  return true;
+}
 
